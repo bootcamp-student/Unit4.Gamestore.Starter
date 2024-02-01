0024ca75dfc8d7934c13c75dbf2348b937240bee
MAC <114251951+bootcamp-student@users.noreply.github.com>
2024-02-01 14:51:28 -0700

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const util = require('../db/util.js');
+const express = require('express');
+const router = express.Router();
+
+// GET /api/health
+router.get('/health', (req, res, next) => {
+    res.send('OK');
+});
+
+// ROUTER: /api/video-games
+router.use('/video-games', require('./videoGames'));
+
+// ROUTER: /api/board-games
+router.use('/board-games', require('./boardGames'));
+
+module.exports = router;
